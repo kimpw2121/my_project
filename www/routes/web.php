32173706c3ccc0577b097094b65402c8edafacf0
Your Name <you@example.com>
2021-11-12 18:25:59 +0900

    업로드

---
+++ www/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('products',ProductController::class);
+Route::resources([
+    'productsx'=>ProductController::class,
+    'productsx2'=>ProductController::class,    
+]);
